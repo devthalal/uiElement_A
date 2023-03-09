d9c50bd291b5424ca1b176bfdf7ba7fb2a78b378
devthalal <thalal.hassan@neoito.com>
2023-03-09 21:49:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Uielement_a from './uiElement_A'
+export default function App() {
+  return (
+    <div>
+      <Uielement_a/>
+    </div>
+  )
+}
---
+++ src/uiElement_A.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Uielement_a = () => {
+  return (
+   <p>Hello from uiElement_A </p>
+  )
+}
+export default Uielement_a
